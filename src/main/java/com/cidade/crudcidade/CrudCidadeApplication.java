d807f4a9aa28ed892463966f585f3a11eb69366a
DANIEL CRISTIANOTI <daniel.cristianoti@hotmail.com>
2023-10-28 10:44:34 -0300

    01- Iniciando Projeto/Pagina Estatica HTML

---
+++ src/main/java/com/cidade/crudcidade/CrudCidadeApplication.java
@@ -0,0 +1,13 @@
+package com.cidade.crudcidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cidade/crudcidade/CrudCidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cidade.crudcidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
