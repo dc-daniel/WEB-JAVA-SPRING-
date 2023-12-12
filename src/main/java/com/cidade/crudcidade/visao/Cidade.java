7bbe37031ab9b67e2d384861bf76b554408c374f
dc-daniel <daniel.cristianoti@hotmail.com>
2023-12-11 21:56:12 -0300

    Criando, alterando e excluindo

--- src/main/java/com/cidade/crudcidade/visao/Cidade.java
+++ src/main/java/com/cidade/crudcidade/visao/Cidade.java
@@ -5,14 +5,19 @@ import lombok.Data;
 @Data
 public final class Cidade {
 
-    private String nome;
-    private String estado;
+    private final String nome;
+    private final String estado;
 
     public Cidade(String nome, String estado) {
         this.nome = nome;
         this.estado = estado;
     }
 
+    public String getEstado() {
+        return estado;
+    }
 
-
+    public String getNome() {
+        return nome;
+    }
 }
